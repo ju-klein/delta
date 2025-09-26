6b36e48339a30c4ace929b3e108f34401fc31006
ju-klein <29863168+ju-klein@users.noreply.github.com>
2025-09-26 18:40:48 +0200

    Enable parallelism by default.

--- run-tests.py
+++ run-tests.py
@@ -84,6 +84,10 @@ def run_sbt_tests(root_dir, test_group, coverage, scala_version=None, shard=None
     cmd += ["-J-XX:+UseG1GC"]
     # 6x the default heap size (set in delta/built.sbt)
     cmd += ["-J-Xmx6G"]
+
+    # ENABLE PARALLELISM BY DEFAULT
+    os.environ["TEST_PARALLELISM_COUNT"] = "16"
+    
     run_cmd(cmd, stream_output=True)
 
 def run_python_tests(root_dir):
